8ba061a25d2f6c5543183e02556587db3613a394
Fernando P Esteves <fpedc13@gmail.com>
2024-06-22 11:15:59 -0300

    projeto teste

---
+++ app.js
@@ -0,0 +1,19 @@
+function consoleClicked() {
+    console.log('O botão foi clicado!');
+}
+
+function iLoveJS() {
+    alert('Eu amo JavaScript!');
+}
+
+function nomeCidade() {
+    let cidade = prompt('Digite uma cidade: ');
+    alert(`Estive em ${cidade} e me lembrei de voce!`);
+}
+
+function somaDoisN() {
+    let n1 = parseInt(prompt('Digite um numero: '));
+    let n2 = parseInt(prompt('Digite outro numero: '));
+    let soma = n1 + n2;
+    alert(`A soma de ${n1} e ${n2} é = ` + soma);
+}
